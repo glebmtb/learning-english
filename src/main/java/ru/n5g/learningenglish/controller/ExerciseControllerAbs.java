aa6f78c8d1e982beab12ad8070ef8ff05299b48e
Belyaev Gleb <Belyaev.Gleb@cg.ru>
2013-12-13 09:43:42 +0400

    для контролеров сделал общий абстрактный класс

---
+++ src/main/java/ru/n5g/learningenglish/controller/ExerciseControllerAbs.java
@@ -0,0 +1,84 @@
+package ru.n5g.learningenglish.controller;
+
+import ru.n5g.learningenglish.Settings;
+import ru.n5g.learningenglish.view.ExerciseView;
+
+/**
+ * @author Belyaev
+ */
+public abstract class ExerciseControllerAbs {
+    protected ExerciseView view;
+
+    protected Integer currentQuestion;
+    protected Integer totalQuestions;
+    protected Integer trueQuestions;
+    protected boolean isEnteredAnswer;
+
+
+    protected ExerciseControllerAbs(ExerciseView view) {
+        this.view = view;
+    }
+
+    public void clickStart() {
+        view.setVisibleStartPage(false);
+        view.setVisibleLessonPage(true);
+
+        currentQuestion = 0;
+        totalQuestions = Settings.numberRepetitions;
+        trueQuestions = 0;
+        view.setTrueQuestions("0");
+        nextQuestion();
+    }
+
+    public void clickNext() {
+        if (currentQuestion.equals(totalQuestions)) {
+            view.setResultTest("правельных ответов: " + trueQuestions + " из: " + totalQuestions);
+            view.setVisibleStartPage(true);
+            view.setVisibleLessonPage(false);
+            return;
+        }
+        nextQuestion();
+    }
+
+    public void clickEnterInInputField() {
+        if (isEnteredAnswer) {
+            clickNext();
+        } else {
+            clickVerify();
+        }
+    }
+
+    protected void nextQuestion() {
+        currentQuestion++;
+        view.clearInformantsResultQuestion();
+        view.setCounterRepetition(currentQuestion + "/" + totalQuestions);
+        view.setPassedQuestions(String.valueOf(currentQuestion - 1));
+        view.setTrueQuestions(trueQuestions.toString());
+        view.setTextForInput(getNewQuestion());
+        isEnteredAnswer = false;
+    }
+
+    public void clickVerify() {
+        if (isEnteredAnswer) {
+            return;
+        }
+        String enteredWord = view.getEnteredText();
+        if (enteredWord != null && !enteredWord.isEmpty()) {
+            enteredWord = enteredWord.trim().toLowerCase();
+        }
+        String rightWord = getRightAnswer().toLowerCase();
+        boolean isRight = rightWord.equals(enteredWord);
+        view.setResultQuestion(isRight);
+        if (isRight) {
+            trueQuestions++;
+        } else {
+            view.setRightAnswer(rightWord);
+        }
+
+        isEnteredAnswer = true;
+    }
+
+    protected abstract String getRightAnswer();
+
+    protected abstract String getNewQuestion();
+}
--- src/main/java/ru/n5g/learningenglish/controller/NumberFrom0To20ExerciseController.java
+++ src/main/java/ru/n5g/learningenglish/controller/NumberFrom0To20ExerciseController.java
@@ -1,85 +1,31 @@
 package ru.n5g.learningenglish.controller;
 
-import ru.n5g.learningenglish.Settings;
-import ru.n5g.learningenglish.util.GenerateRandom;
 import ru.n5g.learningenglish.util.TextForNumber;
 import ru.n5g.learningenglish.view.ExerciseView;
+
+import static ru.n5g.learningenglish.util.GenerateRandom.randomNumberTo20;
 
 /**
  * Gleb Belyaev
  * 12.12.13.
  */
-public class NumberFrom0To20ExerciseController {
-    private ExerciseView view;
-
-    private Integer currentQuestion;
-    private Integer totalQuestions;
-    private Integer trueQuestions;
-    private boolean isEnteredAnswer;
+public class NumberFrom0To20ExerciseController extends ExerciseControllerAbs {
 
     private Integer number;
 
+
     public NumberFrom0To20ExerciseController(ExerciseView view) {
-        this.view = view;
+        super(view);
     }
 
-    public void clickStart() {
-        view.setVisibleStartPage(false);
-        view.setVisibleLessonPage(true);
-
-        currentQuestion = 0;
-        totalQuestions = Settings.numberRepetitions;
-        trueQuestions = 0;
-        view.setTrueQuestions("0");
-        nextQuestion();
+    @Override
+    protected String getRightAnswer() {
+        return TextForNumber.getTextForNumber(number);
     }
 
-    public void clickVerify() {
-        if (isEnteredAnswer) {
-            return;
-        }
-        String enteredWord = view.getEnteredText();
-        if (enteredWord != null && !enteredWord.isEmpty()) {
-            enteredWord = enteredWord.trim().toLowerCase();
-        }
-        String rightWord = TextForNumber.getTextForNumber(number);
-        boolean isRight = rightWord.equals(enteredWord);
-        view.setResultQuestion(isRight);
-        if (isRight) {
-            trueQuestions++;
-        } else {
-            view.setRightAnswer(rightWord);
-        }
-
-        isEnteredAnswer = true;
-    }
-
-    public void clickNext() {
-        if (currentQuestion.equals(totalQuestions)) {
-            view.setResultTest("правельных ответов: " + trueQuestions + " из: " + totalQuestions);
-            view.setVisibleStartPage(true);
-            view.setVisibleLessonPage(false);
-            return;
-        }
-        nextQuestion();
-    }
-
-    public void clickEnterInInputField() {
-        if (isEnteredAnswer) {
-            clickNext();
-        } else {
-            clickVerify();
-        }
-    }
-
-    private void nextQuestion() {
-        currentQuestion++;
-        view.clearInformantsResultQuestion();
-        view.setCounterRepetition(currentQuestion + "/" + totalQuestions);
-        view.setPassedQuestions(String.valueOf(currentQuestion - 1));
-        view.setTrueQuestions(trueQuestions.toString());
-        number = GenerateRandom.randomNumberTo20();
-        view.setTextForInput(number.toString());
-        isEnteredAnswer = false;
+    @Override
+    protected String getNewQuestion() {
+        number = randomNumberTo20();
+        return number.toString();
     }
 }
