f41a3170e43c7cf4a049107d20f96c74cfe601f5
Gleb Belyaev <n5g@ya.ru>
2013-12-30 19:07:55 +0400

    попытка сделать умное изучение

--- src/main/java/ru/n5g/learningenglish/controller/ExerciseControllerAbsImpl.java
+++ src/main/java/ru/n5g/learningenglish/controller/ExerciseControllerAbsImpl.java
@@ -1,11 +1,14 @@
 package ru.n5g.learningenglish.controller;
 
-import ru.n5g.learningenglish.Settings;
 import ru.n5g.learningenglish.util.Mp3Player;
 import ru.n5g.learningenglish.util.StupidRandom;
 import ru.n5g.learningenglish.util.WordRandom;
 import ru.n5g.learningenglish.view.ExerciseView;
 import ru.n5g.learningenglish.words.Words;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Gleb Belyaev
@@ -14,16 +17,19 @@ public class ExerciseControllerAbsImpl extends ExerciseControllerAbs {
     private Words<String, String> words;
     protected String rusWord;
     private WordRandom<String> random;
+    private Map<String, Integer> studiedWordsMap;
+    private int studiedWords;
 
     public ExerciseControllerAbsImpl(ExerciseView view, Words<String, String> words) {
         super(view);
         this.words = words;
         random = new StupidRandom<String>(words);
+        studiedWordsMap = getStudiedWordsMap(words.getListRusWord());
     }
 
     @Override
     protected Integer getTotalQuestions() {
-        return Settings.numberRepetitionsAdverbsOfFrequency;
+        return Integer.MAX_VALUE;
     }
 
     @Override
@@ -38,5 +44,25 @@ public class ExerciseControllerAbsImpl extends ExerciseControllerAbs {
     protected String getNewQuestion() {
         rusWord = random.getRandomWord();
         return rusWord;
+    }
+
+    @Override
+    protected void nextQuestion() {
+        super.nextQuestion();
+        view.setCounterRepetition(studiedWords + "/" + words.size());
+    }
+
+    @Override
+    public void clickStart() {
+        super.clickStart();
+        studiedWords = 0;
+    }
+
+    private Map<String, Integer> getStudiedWordsMap(List<String> list) {
+        Map<String, Integer> smartRandom = new HashMap<String, Integer>();
+        for (String key : list) {
+            smartRandom.put(key, 0);
+        }
+        return smartRandom;
     }
 }
