19743e22f567ee34bd22d69aaa1c4e184c300245
Gleb Belyaev <n5g@ya.ru>
2013-12-30 18:52:09 +0400

    добавлено новое на страницу

--- src/main/java/ru/n5g/learningenglish/view/ExerciseViewImpl.java
+++ src/main/java/ru/n5g/learningenglish/view/ExerciseViewImpl.java
@@ -4,15 +4,29 @@ import ru.n5g.learningenglish.controller.ExerciseControllerAbsImpl;
 import ru.n5g.learningenglish.util.RandomAndTranslate;
 import ru.n5g.learningenglish.words.Words;
 
+import javax.swing.*;
+
 /**
  * @author Belyaev
  */
 public class ExerciseViewImpl extends ExerciseViewAbs {
     protected ExerciseControllerAbsImpl controller;
+    protected JLabel studiedLabel;
 
 
     public ExerciseViewImpl(Words<String, String> words) {
         controller = new ExerciseControllerAbsImpl(this, words);
+    }
+
+    @Override
+    protected void initComponents() {
+        super.initComponents();
+        studiedLabel = new JLabel("Изучено:");
+        studiedLabel.setBounds(20, 15, 55, 20);
+        studiedLabel.setVisible(false);
+        frame.add(studiedLabel);
+
+        counterRepetitionLabel.setBounds(73, 15, 60, 20);
     }
 
     @Override
@@ -33,5 +47,11 @@ public class ExerciseViewImpl extends ExerciseViewAbs {
     @Override
     protected void clickStart() {
         controller.clickStart();
+    }
+
+    @Override
+    public void setVisibleLessonPage(boolean isVisible) {
+        super.setVisibleLessonPage(isVisible);
+        studiedLabel.setVisible(isVisible);
     }
 }
